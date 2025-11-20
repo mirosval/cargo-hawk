a4446979d9ace5035f8283eeff308b89200bbe61
Miroslav Zoricak <1315417+mirosval@users.noreply.github.com>
2025-11-20 20:33:16 +0100

    add crane

--- flake.nix
+++ flake.nix
@@ -3,35 +3,73 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-utils.follows = "flake-utils";
-      };
-    };
+    crane.url = "github:ipetkov/crane";
   };
   outputs =
     {
       self,
       nixpkgs,
+      crane,
       flake-utils,
-      rust-overlay,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
-          inherit system overlays;
+          inherit system;
         };
-        rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        inherit (pkgs) lib;
+        craneLib = crane.mkLib pkgs;
+        snapFilter = path: _type: builtins.match ".*snap$" path != null;
+        snapOrCargo = path: type: (snapFilter path type) || (craneLib.filterCargoSources path type);
+        src = lib.cleanSourceWith {
+          src = ./.;
+          filter = snapOrCargo;
+          name = "source";
+        };
+        commonArgs = {
+          inherit src;
+          strictDeps = true;
+          buildInputs = [ ] ++ lib.optionals pkgs.stdenv.isDarwin [ ];
+
+        };
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+        cargo-hawk = craneLib.buildPackage (
+          commonArgs
+          // {
+            inherit cargoArtifacts;
+          }
+        );
       in
-      with pkgs;
       {
-        devShells.default = mkShell {
-          buildInputs = [ rustToolchain ];
-          packages = [
+        checks = {
+          inherit cargo-hawk;
+
+          cargo-hawk-clippy = craneLib.cargoClippy (
+            commonArgs
+            // {
+              inherit cargoArtifacts;
+              cargoClippyExtraArgs = "--all-targets -- --deny-warnings";
+            }
+          );
+
+          cargo-hawk-fmt = craneLib.cargoFmt {
+            inherit src;
+          };
+
+          cargo-hawk-test = craneLib.cargoTest (
+            commonArgs
+            // {
+              inherit cargoArtifacts;
+            }
+          );
+        };
+
+        packages.default = cargo-hawk;
+
+        devShells.default = craneLib.devShell {
+          checks = self.checks.${system};
+          packages = with pkgs; [
             cargo-insta
             cargo-outdated
             cargo-udeps
